778ff7614d4a3cd6ef579630db9edb27c3bd22ed
Vidyoot Senthil <74870683+vid277@users.noreply.github.com>
2026-01-18 16:19:18 -0500

    feat: add basic placeholder route

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
+from handlers.job_handler import router as job_router
+
 app = FastAPI()
 
+app.include_router(job_router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
 
 @app.get("/health")
 async def health_check():
